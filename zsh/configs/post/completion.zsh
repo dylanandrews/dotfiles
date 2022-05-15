2dfb49606b39f26823a959a82cf88211c2455836
DylanAndrews <dylanandr@gmail.com>
2022-05-15 15:42:14 -0500

    Remove pure prompt and use starship

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -1,10 +1,6 @@
 # load our own completion functions
 fpath=(~/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
 
-# Pure prompt
-autoload -U promptinit; promptinit
-prompt pure
-
 # completion
 autoload -U compinit
 compinit
